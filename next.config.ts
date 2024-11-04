a7cd76704193e601186267b909099bb91fa931c2
Luffeee <sharbelabounader@gmail.com>
2024-11-04 02:40:12 +0200

    added install

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,20 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     domains: [
       "res.cloudinary.com",
       "avatars.githubusercontent.com",
       "lh3.googleusercontent.com"
     ]
-  }
+  },
+  // Add any other Next.js configurations here
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public", // This specifies where the service worker will be generated
+  register: true, // Automatically registers the service worker
+  skipWaiting: true, // Activates the new service worker as soon as it’s available
+  ...nextConfig, // Spread existing configurations here
+});
